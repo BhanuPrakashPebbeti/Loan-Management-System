4701348e0d90b5040da781e357ad26c45ce2d0d6
BhanuPrakashPebbeti <pebbetibhanu2017@gmail.com>
2023-08-14 11:07:29 +0530

    Added routing.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import NavBar from './components/Navbar/NavBar';
 import NavBarNew from './components/Navbar/NavBarNew';
+import Routing from './Routing';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <>
       <NavBarNew />
+      <Routing />
     </>
   );
 }
---
+++ client/src/Routing.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+
+const Routing = () => {
+
+    return(
+      <Routes>
+        {/* Home */}
+        {/* <Route exact path='/' element={<Home />} /> */}
+
+        {/* Projects */}
+        {/* <Route exact path='/items' element={<Items />} /> */}
+        {/* <Route exact path='/additem' element={<AddItem />} /> */}
+
+        {/* Others */}
+        {/* <Route path='*' element={<Error />} /> */}
+        
+      </Routes>
+    )
+  }
+
+export default Routing;
