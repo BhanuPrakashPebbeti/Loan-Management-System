60d6b56d7bb73c5a5b138a2a431bb158d9d55d43
amyyyth <amithjee@gmail.com>
2023-08-18 18:00:04 +0530

    more fixes

--- backend/loan-management/src/main/java/com/wellsfargo/lms/controllers/UserController.java
+++ backend/loan-management/src/main/java/com/wellsfargo/lms/controllers/UserController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ public class UserController {
 	@Autowired
 	private EmployeeService employeeService;
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping
 	public ResponseEntity<?> getAllEmployees(){
 		return ResponseEntity.ok().body(employeeService.getAllUsers());
