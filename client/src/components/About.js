a6c914d7054fcf12318fa501ad79652a08351ecf
Fahadh Kasala <67508953+Fahadh4444@users.noreply.github.com>
2023-08-22 16:45:33 +0530

    Updated About Page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import "./About.css"
 
 const About = () => {
     return (
-        <div className="about-us">
+        <div className="container m-10px about-us">
             <h1>About Loan Management System</h1>
             <p>
                 Welcome to our Loan Management System! We are dedicated to providing
                 financial assistance to our internal employees to help them achieve their
-                goals and meet their financial needs.
-            </p>
-            <p>
-                Our system is designed to streamline the loan application process, making
+                goals and meet their financial needs. Our system is designed to streamline the loan application process, making
                 it easy for our employees to access the financial support they require.
                 We offer a range of loan options with competitive interest rates and
                 flexible repayment plans.
             </p>
+            
             <p>
                 Our team is committed to ensuring a smooth and transparent experience for
                 all employees. Feel free to reach out to our support team if you have any
