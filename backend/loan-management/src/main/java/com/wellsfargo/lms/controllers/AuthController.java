4f32a1077998668fde07ff67068b51b628f91ecd
amyyyth <amithjee@gmail.com>
2023-08-24 00:55:44 +0530

    Change login by name to email

--- backend/loan-management/src/main/java/com/wellsfargo/lms/controllers/AuthController.java
+++ backend/loan-management/src/main/java/com/wellsfargo/lms/controllers/AuthController.java
@@ -59,7 +59,7 @@ public class AuthController {
 	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
 		Authentication authentication = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getPassword()));
+				new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
 
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 		String jwt = jwtUtils.generateJwtToken(authentication);
