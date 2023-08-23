9dd667e1eb2071abd27556aff7b114f6e89c6062
amyyyth <amithjee@gmail.com>
2023-08-23 17:13:32 +0530

    add route for filtering issues with wmployee

--- backend/loan-management/src/main/java/com/wellsfargo/lms/repositories/EmployeeIssueRepo.java
+++ backend/loan-management/src/main/java/com/wellsfargo/lms/repositories/EmployeeIssueRepo.java
@@ -1,5 +1,7 @@
 package com.wellsfargo.lms.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,6 @@ import com.wellsfargo.lms.models.*;
 
 @Repository
 public interface EmployeeIssueRepo extends JpaRepository<EmployeeIssueDetails, String>	 {
-	
+	List<EmployeeIssueDetails> findByEmployee(EmployeeMaster employee);
 
 }
