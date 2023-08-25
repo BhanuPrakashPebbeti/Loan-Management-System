aaf4055daf7b9f53e5d265ce3086b4346e5eee34
amyyyth <amithjee@gmail.com>
2023-08-25 17:40:21 +0530

    merge conflict

--- client/src/components/Loans/LoanIssue.js
+++ client/src/components/Loans/LoanIssue.js
@@ -54,11 +54,8 @@ const LoanIssue = () => {
         console.log(cardId);
         if (status) {
             try {
-<<<<<<< HEAD
-                const res = await axios.post(`${SERVER_URL}/approval/decline?cardId=${cardId}`, {
-=======
+
                 const res = await axios.post(`${SERVER_URL}/approval/decline?cardId=${cardId}`,null, {
->>>>>>> a4052f9ee1bf14eb197a293ee6b523519ac95aca
                     headers: {
                         "authorization": `Bearer ${cookies.get('token')}`,
                         "content-Type": "application/json"
@@ -79,12 +76,7 @@ const LoanIssue = () => {
         console.log(cookies.get('token'));
         if (status) {
             try {
-<<<<<<< HEAD
-                console.log(SERVER_URL);
-                const res = await axios.post(`${SERVER_URL}/approval/new?cardId=${cardId}`, {
-=======
                 const res = await axios.post(`${SERVER_URL}/approval/new?cardId=${cardId}`,null, {
->>>>>>> a4052f9ee1bf14eb197a293ee6b523519ac95aca
                     headers: {
                         "Content-Type": "application/json",
                         "Authorization": `Bearer ${cookies.get('token')}`
