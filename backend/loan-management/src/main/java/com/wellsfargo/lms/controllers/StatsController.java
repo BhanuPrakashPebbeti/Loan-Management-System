126c1e7c197ac4da9bb82fa1aedd862e90bf2324
amyyyth <amithjee@gmail.com>
2023-08-27 10:18:18 +0530

    restrict stats to admin only

--- backend/loan-management/src/main/java/com/wellsfargo/lms/controllers/StatsController.java
+++ backend/loan-management/src/main/java/com/wellsfargo/lms/controllers/StatsController.java
@@ -2,6 +2,7 @@ package com.wellsfargo.lms.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,26 +16,31 @@ public class StatsController {
 	@Autowired
 	StatServices statServices;
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/cards")
 	public ResponseEntity<?> cardStats(){
 		return ResponseEntity.ok(statServices.CardStats());
 	}
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/items")
 	public ResponseEntity<?> itemStats(){
 		return ResponseEntity.ok(statServices.ItemStats());
 	}
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/users")
 	public ResponseEntity<?> userStats(){
 		return ResponseEntity.ok(statServices.UserStats());
 	}
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/issues")
 	public ResponseEntity<?> issueStats(){
 		return ResponseEntity.ok(statServices.IssueStats());
 	}
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/cardtype")
 	public ResponseEntity<?> cardTypeStats(){
 		return ResponseEntity.ok(statServices.CardByTypeStats());
