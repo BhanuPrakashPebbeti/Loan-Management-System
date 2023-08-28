37f97778dfd81d1e86704ea99e833ca11435f4a3
Fahadh Kasala <67508953+Fahadh4444@users.noreply.github.com>
2023-08-28 22:12:56 +0530

    Update Config.js

--- client/src/EditableStuff/Config.js
+++ client/src/EditableStuff/Config.js
@@ -1,4 +1,4 @@
 module.exports = {
     CLIENT_URL: process.env.REACT_APP_CLIENT_URL || "http://localhost:3000",
-    SERVER_URL: process.env.REACT_APP_SERVER_URL || "http://ec2-13-210-14-224.ap-southeast-2.compute.amazonaws.com:8080",
+    SERVER_URL: process.env.REACT_APP_SERVER_URL || "http://localhost:8080",
 }
